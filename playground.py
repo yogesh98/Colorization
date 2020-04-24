ddbe2aa165843a543120b28c0d6fa4d53bf28fe9
unknown <yogesh.patel0221@gmail.com>
2020-04-24 14:46:52 -0400

    added new test image

--- playground.py
+++ playground.py
@@ -11,5 +11,8 @@ if __name__ == "__main__":
     colors = k_means_clustering_on_img(5, im_as_array, 5)
     imFirst = image_first_transform(colors, im, im_as_array)
     imFirst.show('new.jpg')
+
+
+
     #print(colors)
 
